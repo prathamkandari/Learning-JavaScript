f2101470c6471f51ddd1c62b10276d610d8e378d
prathamkandari <prathamkandari123@gmail.com>
2023-06-02 23:27:14 +0530

    Strings

---
+++ StringsPractice.js
@@ -0,0 +1,10 @@
+// Practice 1
+// let str = "Please give me Rs 1000"
+// let amount = Number.parseInt(str.slice(18)) //Cauz it would be string elsewhere
+// console.log(amount)
+// console.log(typeof amount)
+
+// Practice 2
+
+let str = "Were you able to do it"
+console.log(str[4]) // Strings are immutable and therefore they cannot be changed
